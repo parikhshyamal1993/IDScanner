5fef350e11f580e3d6e7a2fd30e65538ef99761a
shyamal <parikhshyamal1993@gmail.com>
2021-10-27 10:22:25 +0530

    adhar added

--- source/IDProcessor.py
+++ source/IDProcessor.py
@@ -159,35 +159,77 @@ class AadharExtraction():
         panImage = cv2.cvtColor(panColor,cv2.COLOR_BGR2GRAY)
         #panImage = panImage / meanImg
         print("panImage",panImage.shape)
-        kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(13,3))
-        dst = cv2.morphologyEx(panImage, cv2.MORPH_GRADIENT,kernel)
-        
-        #_, dst = cv2.threshold(panImage,70,255,cv2.THRESH_BINARY_INV)
-        #dst = cv2.dilate(mask,self.kernal,iterations = 1)
-        #dst = cv2.bitwise_not(dst)
+        thresh1 = cv2.adaptiveThreshold(panImage, 255, cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY, 199, 10)
          
-        cv2.imshow("dst",cv2.resize(dst,(600,375)))
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        kernel_ = cv2.getStructuringElement(cv2.MORPH_RECT,(31,1))
-        clossing = cv2.morphologyEx(dst,cv2.MORPH_CLOSE,kernel_)
-        _, dst = cv2.threshold(clossing,90,255,cv2.THRESH_BINARY)
-        contours , hierarchy = cv2.findContours(dst,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_NONE)
+        # cv2.imshow("dst",cv2.resize(thresh1,(600,375)))
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+        kernel_ = cv2.getStructuringElement(cv2.MORPH_RECT,(23,1))
+        clossing = cv2.morphologyEx(thresh1,cv2.MORPH_OPEN,kernel_)
+        
+        #clossing[clossing<140] = 0
+        contours , hierarchy = cv2.findContours(clossing,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_NONE)
         allBoxes = []
         typeIDList = []
         for cnt , high in zip(contours,hierarchy[0]):
             x,y,w,h = cv2.boundingRect(cnt)
             if h > 20 and w >30 and h <60:
                 cv2.rectangle(panColor,(x,y),(x+w,y+h),(0,255,100),3)
-                cells = panImage[y:y+h,x:x+w]
-                #gray = cv2.cvtColor(cells,cv2.COLOR_BGR2GRAY)
+                cells = panColor[y:y+h,x:x+w]
                 data = image_to_string(cells,config='--psm 7')
-                allBoxes.append([data,[x,y,x+w,y+h]])
+                allBoxes.append([re.sub("\n\x0c","",data),[x,y,x+w,y+h]])
         cv2.imshow("Binary",cv2.resize(panColor,(600,375)))
         cv2.waitKey(0)
         cv2.destroyAllWindows()
         allBoxes.reverse()
         return allBoxes
+
+    def run(self,image):
+        data = self.AadharExtract(image)
+        output = {}
+        HOCR = {}
+        output["Aadhar number"] = ""
+        IDIdentityList = ["Government of India" ,"Government","India"]
+        GenderIdentityList = ["male", "female" ,"transgender"]
+        DateList = ["Birth","Year","YoB"]
+        FatherList = ["Father"]
+        IncomeLine = 0
+        PanCard = 0
+        for i in range(len(data)):
+            print("items :",data[i][0])
+            for items in IDIdentityList:
+
+                if re.findall(items.lower() , data[i][0].lower()):
+                    output["Name"] = data[i+2][0]
+                    HOCR["Name"] = data[i+2][1]
+                    #print("Fathers Name",data[i+5])
+                    break
+            for items in GenderIdentityList:
+                if re.findall(items.lower() , data[i][0].lower()):
+                    try:
+                        gender = data[i][0].split("/")[-1]
+                    except:
+                        gender = data[i][0]
+
+                    output["gender"] = gender
+                    HOCR["gender"] = items
+                    #print("Fathers Name",data[i+5])
+                    break
+            for items in DateList:
+                if re.findall(items.lower() , data[i][0].lower()):
+                    print("date",data[i][0])
+                    date = "".join([inte for inte in data[i][0].split() if inte.isdigit()])
+                    output["date"] = date
+                    HOCR["date"] = data[i][1]
+                    #print("Fathers Name",data[i+5])
+                    break
+            if re.sub(" ", "",data[i][0]).isdigit():
+                print("numbers",data[i][0])
+                output["Aadhar number"] += data[i][0]
+
+
+        return output
+
 
 class PassportExtractor():
     def __init__(self) -> None:
@@ -206,6 +248,5 @@ class IDextract():
 if __name__ == "__main__":
     pan = PanCardExtractor()
     aadhar = AadharExtraction()
-    outPuts = aadhar.AadharExtract(sys.argv[1])
-    for srgs in outPuts:
-        print("outPuts :" , srgs)
+    outPuts = aadhar.run(sys.argv[1])
+    print("output :",outPuts)
